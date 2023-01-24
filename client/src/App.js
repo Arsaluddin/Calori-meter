831bc82b98098a9f85a1c197a5268a7886767ee8
arsal uddin <arsaluddin134@gmail.com>
2023-01-24 23:56:17 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-// import logo from './logo.svg';
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
 import './App.css';
+import Navbar from "./components/Navbar";
+import AddFood from "./components/AddFood";
+import EditFood from "./components/EditFood";
+import DisplayFood from "./components/DisplayFoodlist";
+import AddUser from "./components/AddUser";
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+      <>
+
+        <Router>
+          <Navbar/>
+
+          <Routes>
+            <Route path="/" element={<DisplayFood/>} />
+            <Route path="/edit/:id" element={<EditFood/>} />
+            <Route path="/create" element={<AddFood/>} />
+            <Route path="/user" element={<AddUser/>} />
+          </Routes>  
+        </Router>       
+
+      </>
   );
 }
 
