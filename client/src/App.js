56e1f8b175cba2833d141ef1c92730b29c07061d
arsal uddin <arsaluddin134@gmail.com>
2023-01-26 23:31:03 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
+import {BrowserRouter , Routes , Route} from "react-router-dom"
 import './App.css';
-import Navbar from "./components/Navbar";
-import AddFood from "./components/AddFood";
+import Navbar from "./components/Navbar/Navbar.js";
+import AddFood from "./components/AddFood/Addfood.js";
 import EditFood from "./components/EditFood";
 import DisplayFood from "./components/DisplayFoodlist";
 import AddUser from "./components/AddUser";
 
 
-function App() {
+const App = () => {
   return (
       <>
-
-        <Router>
+        <BrowserRouter>
           <Navbar/>
-
+          <br/>
           <Routes>
             <Route path="/" element={<DisplayFood/>} />
             <Route path="/edit/:id" element={<EditFood/>} />
             <Route path="/create" element={<AddFood/>} />
             <Route path="/user" element={<AddUser/>} />
           </Routes>  
-        </Router>       
+        </BrowserRouter>       
 
       </>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,31 @@ const Navbar = () => {
 
     return (
         <>
-        
+        <h1>arsal</h1>
+           <nav>
+             <div className="container">
+                <Link to="/"></Link>
+
+                <Link to="/"></Link>
+
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/"></Link>
+                            <button>Calori info</button>
+                        </li>
+                        <li>
+                            <Link to="/create"></Link>
+                            <button>Add food</button>
+                        </li>
+                        <li>
+                            <Link to="/user"></Link>
+                            <button>Add user</button>
+                        </li>
+                    </ul>
+                </div>
+             </div>
+           </nav>
         </>
     )
 }
