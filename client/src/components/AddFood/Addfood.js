4f1c2af0ab7579ab3abde4404611d5f2aa8c7eb5
arsal uddin <arsaluddin134@gmail.com>
2023-02-01 22:44:49 +0530

    .

--- client/src/components/AddFood/Addfood.js
+++ client/src/components/AddFood/Addfood.js
@@ -5,7 +5,17 @@ const Addfood = () => {
 
     return (
         <>
-        
+            <div className="container">
+                <div className="image">
+                    <img  src="https://user-images.githubusercontent.com/37651620/142764215-78f5b75f-4871-451e-9a4d-dd77cc667fc5.png" alt="" height={100} width={100} ></img>
+                </div>
+                <div className="form">
+
+                </div>
+                <div className="date">
+
+                </div>
+            </div>
         </>
     )
 }
