a909f7c7699099f9db14462709d427cfc5af66db
arsal uddin <arsaluddin134@gmail.com>
2023-02-05 20:10:20 +0530

    .EditFood

--- client/src/components/AddUser/Adduser.js
+++ client/src/components/AddUser/Adduser.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 
 const Adduser = () => {
 
+    const [username,setUsername] = useState("");
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+const handleSubmit = (e) => {
+      e.preventFefault();
+      const user = {
+        username,
+      };
+      console.log(user);
+      axios
+      .post("http://localhost:5000/users/add",user)
+      .then((res) => console.log(res.data));
+      setUsername("");
+}
+
+
     return (
         <>
-        
+           <div className="container">
+               <div className="image">
+                  <img src="https://user-images.githubusercontent.com/37651620/142767072-ff777861-7ee9-4355-b48e-a624e8de085b.png" height={150} alt="logo"/>
+               </div>
+               <form typeof="Submit" onSubmit={handleSubmit}>
+                   <label> 👨‍✈️ Username</label>
+                   <input type="text" value={username} onChange={handleUsername}></input>
+               </form>
+           </div>
         </>
     )
 }
--- client/src/components/EditFood/EditFood.js
+++ client/src/components/EditFood/EditFood.js
@@ -1,11 +1,127 @@
+import { useState,useRef,useEffect } from "react";
 import React from "react";
-// import { Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
-const EditFood = () => {
+
+const EditFood = (props) => {
+    const [username,setUsername] = useState("");
+    const [description,setDescription] = useState("");
+    const [calories,setCalories] = useState("");
+    const [date,setDate] = useState(new Date());
+    const [users,setUsers] = useState([]);
+    const userInputRef = useRef("userInput");
+
+    
+    useEffect(() => {
+
+       axios
+           .get("http://localhost:5000/calorie/" + props.match.params.id)
+           .then((res) => {
+                setUsername(res.data.username);
+                setCalories(res.data.calories);
+                setDescription(res.data.description);
+                setDate(new Date(res.data.date));
+           })
+           .catch((err) => {
+              console.log(err);
+           });
+
+         axios
+             .get("http://localhost:5000/users/")
+             .then((res) => {
+                if(res.data.length > 0){
+                    setUsers(res.data.map((user) => user.username));
+                    setUsername(res.data[0].username);
+                }
+             })
+             .catch((err) => {
+                console.log(err);
+             })  
+
+    },[props.match.params.id])
+
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handleCalories = (e) => {
+        setCalories(e.target.value);
+    }
+
+    const handleDescription = (e) => {
+        setDescription(e.target.value);
+    }
+
+    const handleDate = (date) => {
+        setDate(date);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const food = {
+            username,
+            description,
+            calories,
+            date,
+        };
+
+        console.log(food);
+
+        axios
+            .post("http://localhost:5000/calorie/update",food)
+            .then((res)=>console.log(res.data));
+
+            window.location="/";
+    }
 
     return (
         <>
-        
+           <div className="container">
+              <div className="image">
+                <img src="https://user-images.githubusercontent.com/37651620/142764215-78f5b75f-4871-451e-9a4d-dd77cc667fc5.png" height={150}></img>
+              </div>
+              <form typeof="submit" onSubmit={handleSubmit}>
+                 <div className="username">
+                    <label>👨‍🎓 Username</label>
+                    <select 
+                    ref={userInputRef} 
+                    value={username} 
+                    onChange={handleUsername}>
+                        {users.map((user) => {
+                            return(
+                                <option key={user} value={user}>
+                                  {user}
+                                </option>
+                            )
+                        })}
+                    </select>
+                 </div>
+                 <div className="food-info">
+                    <label>🥗 Food Info</label>
+                    <input 
+                    type="text" 
+                    value={description} 
+                    onChange={handleDescription}></input>
+                 </div>
+                 <div className="calories">
+                    <lable> 💪 calorie</lable>
+                    <input 
+                    type="text"
+                    value={calories} 
+                    onChange={handleCalories}></input>
+                 </div>
+              </form>
+              <div className="date">
+                <label>Date: </label>
+                <div>
+                    <DatePicker selected={date} onChange={handleDate}></DatePicker>
+                </div>
+              </div>
+           </div>
         </>
     )
 }
